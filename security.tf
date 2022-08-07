211ae1677bc11d4950df0b4236d55be7b45c84a9
Ali Pourtarki <93008134+Alipk60@users.noreply.github.com>
2022-08-07 19:13:40 -0400

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "web_traffic" {
+ name        = "Allow web traffic"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port     = 0
+    to_port       = 0
+    protocol      = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port     = 0
+    to_port       = 0
+    protocol      = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 0   
+    to_port     = 65535
+    protocol    = "TCP"
+    cidr_blocks = ["54.161.39.189/190"]
+  }
+}
