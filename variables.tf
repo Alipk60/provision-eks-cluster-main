3a8afb01242b540807b48077b17807aec44bc4c4
Ali Pourtarki <93008134+Alipk60@users.noreply.github.com>
2022-08-07 14:14:08 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,3 +11,4 @@ variable "aws_secret_key" {
 }
 variable "aws_token" {
   default = "FwoGZXIvYXdzEKn//////////wEaDG0gxv+rjO0UII+6TyK0AYZ0HF+MIPk5OGye6A34V+fUyUzwhlivcWXeL0wgirev7q20D7BHETx5LX79NFhfu5ujUTHdFbk3fJWrs7gXCrJwf7rRMZHg0+K5Pnk2aV/IF01HPyTZ4A/BfWtVz2gNURLfntevT86MDqhRE+5khMlNYcWjXkXi1d64FtAju/3go3x6VcH6KvXa/Njm4LtKcwM+RBffLuxIvXyEpQ8F+A/dmOAqhlep3H2I87Y05iJe6e0btyiRwr+XBjItfu6Zh77fD4WOnN/PGG7CQYFiNx2xVAiNbLS0OSokz+y96ZTewVDd0B2YnXNu"
+}
