5a1373bea227511ea97ef23951aad85527661333
Ali Pourtarki <93008134+Alipk60@users.noreply.github.com>
2022-08-09 16:36:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,24 +13,78 @@ esource "aws_instance" "ubuntu" {
     private_key = file(var.path_to_private_key)
     user        = var.instance_username
   }
+#provisioner "local-exec" {
+#      command = "echo [master nodes] \n${aws_instance.ubuntu.*.public_ip} >> ${var.host_file}"
+#    }
 }
+
+# Give Terraform cycle
 resource "null_resource" "ubuntu" {
   count = length(var.instance_count)
-  triggers = {
+
     #config_file = templatefile("${path.module}/sys/etc/mysql/mariadb.conf/galera.cnf", {
      # all_addresses = hcloud_server.galera_node[*].ipv4_address
      # this_address  = hcloud_server.galera_node[count.index].ipv4_address
      # this_id       = hcloud_server.galera_node[count.index].id
   # })
-  }
+
+  provisioner "local-exec" {
+      command = "echo [master node] >> ${var.host_file}"
+    }
   provisioner "local-exec" {
       command = "echo ${element(aws_instance.ubuntu.*.public_ip, count.index)} >> ${var.host_file}"
     }
-
- # provisioner "local-exec" {
+  provisioner "local-exec" {
+      command = "echo [worker node] >> ${var.host_file}"
+    }
+  provisioner "local-exec" {
+      command = "echo ${element(aws_instance.ubuntu.*.public_ip, count.index+1)} >> ${var.host_file}"
+    }
+  provisioner "local-exec" {
+      command = "echo ${element(aws_instance.ubuntu.*.public_ip, count.index+2)} >> ${var.host_file}"
+    }
+# provisioner "local-exec" {
  #   command = "sleep 120; ansible-playbook -u ubuntu --private-key ./project1.pem -i hosts playbook.yml"
  # }
+}
+# Create elastic IP and assosiated to instances
+resource "aws_eip" "lb" {
+     instance = "${element(aws_instance.ubuntu.*.id,count.index)}"
+     count    = var.instance_count
+     vpc      = true
+     tags = {
+          Name = "eip-ubuntu-${count.index+1}"
+            }
+       }
+# Create a new load balancer
 
+resource "aws_elb" "bar" {
+  name               = "ubuntu-terraform-elb"
+  availability_zones = ["us-east-1a"]
+  security_groups    = ["${aws_security_group.web_traffic.id}"]
+
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+   }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+  instances                   = "${aws_instance.ubuntu.*.id}"
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "foobar-terraform-elb"
+  }
 }
 
 output "ec2_global_ips" {
